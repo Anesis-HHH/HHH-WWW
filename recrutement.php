598dd8d745146b1004068f9896c64d30c04b3ef9
Albirew <rad1cal_edward@yahoo.fr>
2017-12-12 19:06:26 +0100

    typo
    correction de fautes signalées par mail

--- recrutement.php
+++ recrutement.php
@@ -11,8 +11,8 @@ include 'include/head.php';
 						<div id="pagesstatiques">
 							<p>
 								Le recrutement dans l'équipe se fait uniquement via IRC et non par mail. <br/><br/>
-								Lors du recrutement vous serez mis en contact avec un membre de la team qui sera votre référent pendant toute la durée du test de recrutement et au début pour vous donnez les clés de la team si vous réussissez le test d'entrée.<br/><br/>
-								Les tests de recrutement ont été élaborés dans le but de toucher toute les parcelles du scantrad de la spécialité que vous aurez choisit.<br/><br/>
+								Lors du recrutement vous serez mis en contact avec un membre de la team qui sera votre référent pendant toute la durée du test de recrutement et au début pour vous donner les clés de la team si vous réussissez le test d'entrée.<br/><br/>
+								Les tests de recrutement ont été élaborés dans le but de toucher toute les parcelles du scantrad de la spécialité que vous aurez choisi.<br/><br/>
 								Seul le pôle développement n'est pas sujet à un test, vous ne pouvez pas être recruté sur votre propre envie. En effet, tous les développeur ont été recruté au fil du temps sur une base de confiance mutuelle et une expertise avérée.
 								
 							</p>
